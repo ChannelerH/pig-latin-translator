feb8094c07de1ce07dc74406c3486265a630a3b5
huaerze <>
2025-02-25 23:05:05 +0800

    edit code

--- src/App.tsx
+++ src/App.tsx
@@ -324,6 +324,7 @@ function App() {
             <Github size={20} />
             View on GitHub
           </a>
+          <span>.<span/>
           <a href="https://luminabrush.site/"
            target="_blank"
            rel="noopener noreferrer"
